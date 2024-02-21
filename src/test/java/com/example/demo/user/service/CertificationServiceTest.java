b8d408a3a22c875f7bfc0b265614e887bf3ecdd1
dev_one <micool1030@gmail.com>
2024-02-21 09:04:43 +0900

    [test] CertificationService 테스트 작업 - FakeMailSender 처리

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CertificationServiceTest {
+
+    @DisplayName("이메일과 컨텐츠가 제대로 만들어져서 보내진다.")
+    @Test
+    void send() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        String mail = "member1@test.com";
+        String certificationCode = "aaaa-aaaa-aaaa-aaaa";
+        int userId = 1;
+        certificationService.send(mail, userId, certificationCode);
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo(mail);
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/" + userId + "/verify?certificationCode=" + certificationCode);
+    }
+
+}
